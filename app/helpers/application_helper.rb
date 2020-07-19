c0267563a3db04dbc0ee64079882cab28a54a62d
zmartin02740 <martin.zachary.e@gmail.com>
2020-07-19 16:50:45 -0400

    login pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if !current_user.is_a?(GuestUser)
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     else 
-      (link_to "Create Account", new_user_registration_path) + '<br />'.html_safe +
-      (link_to "Sign in", new_user_session_path)
+      (link_to "Create Account", new_user_registration_path, class: style) + " ".html_safe +
+      (link_to "Sign in", new_user_session_path, class: style)
     end
   end
 
