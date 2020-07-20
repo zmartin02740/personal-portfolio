fc94ef18c6f29cc328ce2cbb6effbe65b6596486
zmartin02740 <martin.zachary.e@gmail.com>
2020-07-20 14:17:59 -0400

    css styling changes and strcutural changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,43 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: about_path,
+        title: "About"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      },
+      {
+        url: portfolios_path,
+        title: "Portfolio"
+      },
+      {
+        url: blogs_path,
+        title: "Blog"
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ""
+    
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}'' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
