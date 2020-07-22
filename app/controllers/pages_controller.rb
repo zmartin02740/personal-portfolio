8fdc6d5597b3c0a773d07302dba0e2cce1ef6bd1
zmartin02740 <martin.zachary.e@gmail.com>
2020-07-22 16:22:43 -0400

    made some updates to the about me page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,53 @@ class PagesController < ApplicationController
   end
 
   def about
-    @skills = Skill.all
+    # @skills = Skill.all
+    @skills = [
+      {
+        title: "Javascript",
+        percent_utilized: 85
+        },
+        {
+        title: "React.js",
+        percent_utilized: 85
+        },
+        {
+        title: "Ruby",
+        percent_utilized: 85
+        },
+        {
+        title: "Python",
+        percent_utilized: 85
+        },
+        {
+        title: "SQL",
+        percent_utilized: 85
+        },
+        {
+        title: "PostgreSQL",
+        percent_utilized: 85
+        },
+        {
+        title: "Ruby on Rails",
+        percent_utilized: 85
+        },
+        {
+        title: "MongoDB",
+        percent_utilized: 85
+        },
+        {
+        title: "AWS S3",
+        percent_utilized: 85
+        },
+        {
+        title: "React Native",
+        percent_utilized: 85
+        },
+        {
+        title: "Sketch",
+        percent_utilized: 85
+        }
+      ]
   end
 
   def contact
